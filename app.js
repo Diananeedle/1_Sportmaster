5cd84ab5083e0388111bceda526fd4d1ffcb6855
diananeedle <diananeedle@gmail.com>
2019-10-10 15:31:46 +0400

    add if/else

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log('worked');
+
+//за каждый полную 1000 - 100 бонус
+
+const purchase = 10000;
+let bonus;
+
+if (purchase < 1000){
+    bonus = 'Purchase less than 1000rub - no bonuses';
+    console.log(bonus);
+} else if (purchase < 10000) {
+    bonus = Math.floor(purchase/1000);
+    console.log('Sum of your bonuses: ', bonus*100);
+} else if (purchase < 100000) {
+    bonus = Math.floor(purchase/1000);
+    console.log('Sum of your bonuses: ', bonus*100);
+} else {
+    bonus = 'Please, buy our company';
+    console.log(bonus);
+}
+
